57f72b03ac3485653aca8d6fa11b2fda2e67db45
Cristian Guerra <cristian@digithaigroup.com>
2021-03-18 11:06:31 +0700

    Initial Commit

---
+++ RecipeList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeList
+//
+//  Created by Cristian Guerra on 18/3/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Cristian Guerra on 18/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
