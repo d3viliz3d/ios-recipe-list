591694455608a023413d4c23a9912c9d845c929f
Cristian Guerra <cristian@digithaigroup.com>
2021-03-23 11:20:47 +0700

    Implement featured view

--- RecipeList/Views/RecipeFeaturedView.swift
+++ RecipeList/Views/RecipeFeaturedView.swift
@@ -12,12 +12,54 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading){
+            
+            Text("Featured recipes")
+                .font(.largeTitle)
+                .padding(.leading)
+            
+            GeometryReader { geo in
+                TabView {
+                    ForEach (0..<model.recipes.count) { index in
+                        if model.recipes[index].featured {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .foregroundColor(.black)
+                                        .padding(5)
+                                }
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(15)
+                            .shadow(color: .black, radius: 10, x: -5, y: 5)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            
+            VStack (alignment: .leading){
+                Text("Prep time")
+                Text("1 hour")
+                Text("Highlight")
+                Text("Healthy, hearty")
+            }
+            .padding(.leading)
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
