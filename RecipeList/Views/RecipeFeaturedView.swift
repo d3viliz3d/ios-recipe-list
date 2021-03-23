2006065ee673aac5d83dabfbd37cdd8b68c98776
Cristian Guerra <cristian@digithaigroup.com>
2021-03-23 15:33:56 +0700

    Add featured button and highlights

--- RecipeList/Views/RecipeFeaturedView.swift
+++ RecipeList/Views/RecipeFeaturedView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
+    @State var isDetailViewShowing = false
+    @State var tabIndex = 1
     
     var body: some View {
         
@@ -20,21 +22,33 @@ struct RecipeFeaturedView: View {
                 .padding(.leading)
             
             GeometryReader { geo in
-                TabView {
+                TabView (selection: $tabIndex) {
                     ForEach (0..<model.recipes.count) { index in
                         if model.recipes[index].featured {
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                VStack(spacing: 0) {
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
-                                        .foregroundColor(.black)
-                                        .padding(5)
+                            Button(
+                                action: {
+                                    self.isDetailViewShowing = true
+                                },
+                                label : {
+                                    ZStack {
+                                        Rectangle()
+                                            .foregroundColor(.white)
+                                        VStack(spacing: 0) {
+                                            Image(model.recipes[index].image)
+                                                .resizable()
+                                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                                .clipped()
+                                            Text(model.recipes[index].name)
+                                                .foregroundColor(.black)
+                                                .padding(5)
+                                        }
+                                    }
                                 }
+                                
+                            )
+                            .tag(index)
+                            .sheet(isPresented: $isDetailViewShowing) {
+                                RecipeDetailView(recipe: model.recipes[index])
                             }
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             .cornerRadius(15)
@@ -48,9 +62,15 @@ struct RecipeFeaturedView: View {
             
             VStack (alignment: .leading){
                 Text("Prep time")
-                Text("1 hour")
+                    .font(.headline)
+                Text(model.recipes[tabIndex].prepTime)
                 Text("Highlight")
-                Text("Healthy, hearty")
+                    .font(.headline)
+                HStack {
+                    ForEach (0..<model.recipes[tabIndex].highlights.count) { index in
+                        Text(model.recipes[tabIndex].highlights[index])
+                    }
+                }
             }
             .padding(.leading)
         }
